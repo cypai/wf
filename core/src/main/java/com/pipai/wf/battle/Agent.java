195fb57671bfd558f95e29c7af635baf4d648382
Charlie Pai <mail4charliepai@gmail.com>
2015-02-11 19:33:13 -0800

    Added Agent

--- core/src/main/java/com/pipai/wf/battle/Agent.java
+++ core/src/main/java/com/pipai/wf/battle/Agent.java
@@ -3,4 +3,21 @@ package com.pipai.wf.battle;
 
 public class Agent {
 	
+	public enum State {NEUTRAL, KO};
+	
+	private int ap;
+	private int mobility;
+	
+	public Agent() {
+		
+	}
+	
+	public int getAP() { return this.ap; }
+	public void setAP(int ap) { this.ap = ap; }
+	
+	public void performAction() {
+		
+	}
+	
+	
 }
