20f8be9c055e6d2ffea70369a5c6c378f23d772f
Charlie Pai <mail4charliepai@gmail.com>
2015-08-25 00:46:01 -0700

    Added an ability factory

---
+++ core/src/main/java/com/pipai/wf/unit/ability/AbilityFactory.java
@@ -0,0 +1,18 @@
+package com.pipai.wf.unit.ability;
+
+public class AbilityFactory {
+	
+	public static Ability clone(Ability a) {
+		return createAbility(a.type());
+	}
+	
+	public static Ability createAbility(AbilityType t) {
+		switch (t) {
+		case REGENERATION:
+			return new RegenerationAbility(1);
+		default:
+			return null;
+		}
+	}
+	
+}
