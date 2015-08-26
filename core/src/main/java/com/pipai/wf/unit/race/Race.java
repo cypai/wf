d92fd7a427a94893eba87191b1e2180cd4ab1401
Cheng-Yu Pai <mail4charliepai@gmail.com>
2015-08-26 15:14:35 -0700

    Fixed Race.java to use AgentStateFactory

--- core/src/main/java/com/pipai/wf/unit/race/Race.java
+++ core/src/main/java/com/pipai/wf/unit/race/Race.java
@@ -1,13 +1,14 @@
 package com.pipai.wf.unit.race;
 
 import com.pipai.wf.battle.agent.AgentState;
+import com.pipai.wf.battle.agent.AgentStateFactory;
 
 public enum Race {
 	
-	HUMAN(AgentState.statsOnlyState(7, 5, 2, 12, 65, 0)),
-	FAIRY(AgentState.statsOnlyState(4, 10, 2, 13, 65, 0)),
-	CAT(AgentState.statsOnlyState(4, 3, 3, 14, 65, 5)),
-	FOX(AgentState.statsOnlyState(5, 10, 2, 13, 65, 0));
+	HUMAN(AgentStateFactory.statsOnlyState(7, 5, 2, 12, 65, 0)),
+	FAIRY(AgentStateFactory.statsOnlyState(4, 10, 2, 13, 65, 0)),
+	CAT(AgentStateFactory.statsOnlyState(4, 3, 3, 14, 65, 5)),
+	FOX(AgentStateFactory.statsOnlyState(5, 10, 2, 13, 65, 0));
 	
 	private AgentState baseline;
 	
