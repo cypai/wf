9098b2cad220d5539f055ea33dcffa944c697453
Charlie Pai <mail4charliepai@gmail.com>
2015-08-29 21:26:26 -0700

    Added exception to check if a cell exists when adding Agent

--- core/src/main/java/com/pipai/wf/battle/map/BattleMap.java
+++ core/src/main/java/com/pipai/wf/battle/map/BattleMap.java
@@ -69,7 +69,7 @@ public class BattleMap implements BattleEventLoggable {
 	public BattleMapCell getCell(GridPosition pos) {
 		return this.cellMap.get(this.coordinatesToKey(pos));
 	}
-	
+
 	public BattleMapCell getCellInDirection(GridPosition pos, Direction d) {
 		GridPosition cellPos = null;
 		switch (d) {
@@ -85,7 +85,7 @@ public class BattleMap implements BattleEventLoggable {
 		case S:
 			cellPos = new GridPosition(pos.x, pos.y-1);
 			break;
-		} 
+		}
 		return this.cellMap.get(this.coordinatesToKey(cellPos));
 	}
 
@@ -96,6 +96,9 @@ public class BattleMap implements BattleEventLoggable {
 	public void addAgent(AgentState state) {
 		Agent agent = new Agent(this, state);
 		BattleMapCell cell = this.getCell(agent.getPosition());
+		if (cell == null) {
+			throw new IllegalArgumentException("Cell " + agent.getPosition().toString() + " does not exist");
+		}
 		cell.setAgent(agent);
 		agent.register(log);
 		this.agents.add(agent);
@@ -115,9 +118,9 @@ public class BattleMap implements BattleEventLoggable {
 	private Vector2 gridCenter(GridPosition pos) {
 		return new Vector2(pos.x+0.5f, pos.y+0.5f);
 	}
-	
+
 	public class Supercover {
-		
+
 		public class CornerPair {
 			public GridPosition a, b;
 			public CornerPair(GridPosition a, GridPosition b) {
@@ -125,27 +128,27 @@ public class BattleMap implements BattleEventLoggable {
 				this.b = b;
 			}
 		}
-		
+
 		public ArrayList<GridPosition> supercover;
 		public ArrayList<CornerPair> corners;
-		
+
 		public Supercover() {
 			supercover = new ArrayList<GridPosition>();
 			corners = new ArrayList<CornerPair>();
 		}
-		
+
 		public void remove(GridPosition pos) {
 			supercover.remove(pos);
 		}
-		
+
 		public void add(GridPosition pos) {
 			supercover.add(pos);
 		}
-		
+
 		public void addCornerPair(GridPosition a, GridPosition b) {
 			corners.add(new CornerPair(a, b));
 		}
-		
+
 	}
 
 	public Supercover supercover(GridPosition a, GridPosition b) {
@@ -154,66 +157,68 @@ public class BattleMap implements BattleEventLoggable {
 
 	private Supercover supercover(Vector2 a, Vector2 b) {
 		Supercover points = new Supercover();
-		int i;               // loop counter 
-		int ystep, xstep;    // the step on y and x axis 
-		int error;           // the error accumulated during the increment 
-		int errorprev;       // *vision the previous value of the error variable 
-		int y = (int)a.y, x = (int)a.x;  // the line points 
-		int ddy, ddx;        // compulsory variables: the double values of dy and dx 
-		int dx = (int)b.x - (int)a.x; 
-		int dy = (int)b.y - (int)a.y; 
-		// NB the last point can't be here, because of its previous point (which has to be verified) 
-		if (dy < 0){ 
-			ystep = -1; 
-			dy = -dy; 
-		}else 
-			ystep = 1; 
-		if (dx < 0){ 
-			xstep = -1; 
-			dx = -dx; 
-		}else 
-			xstep = 1; 
-		ddy = 2 * dy;  // work with double values for full precision 
-		ddx = 2 * dx; 
-		if (ddx >= ddy){  // first octant (0 <= slope <= 1) 
-			// compulsory initialization (even for errorprev, needed when dx==dy) 
-			errorprev = error = dx;  // start in the middle of the square 
-			for (i=0 ; i < dx ; i++){  // do not use the first point (already done) 
-				x += xstep; 
-				error += ddy; 
-				if (error > ddx){  // increment y if AFTER the middle ( > ) 
-					y += ystep; 
-					error -= ddx; 
-					// three cases (octant == right->right-top for directions below): 
-					if (error + errorprev < ddx)  // bottom square also 
-						points.add(new GridPosition(x, y-ystep)); 
-					else if (error + errorprev > ddx)  // left square also 
-						points.add(new GridPosition(x-xstep, y)); 
-					else{  // corner: bottom and left squares also 
-						points.addCornerPair(new GridPosition(x, y-ystep), new GridPosition(x-xstep, y)); 
+		int i;               // loop counter
+		int ystep, xstep;    // the step on y and x axis
+		int error;           // the error accumulated during the increment
+		int errorprev;       // *vision the previous value of the error variable
+		int y = (int)a.y, x = (int)a.x;  // the line points
+		int ddy, ddx;        // compulsory variables: the double values of dy and dx
+		int dx = (int)b.x - (int)a.x;
+		int dy = (int)b.y - (int)a.y;
+		// NB the last point can't be here, because of its previous point (which has to be verified)
+		if (dy < 0){
+			ystep = -1;
+			dy = -dy;
+		} else {
+			ystep = 1;
+		}
+		if (dx < 0){
+			xstep = -1;
+			dx = -dx;
+		} else {
+			xstep = 1;
+		}
+		ddy = 2 * dy;  // work with double values for full precision
+		ddx = 2 * dx;
+		if (ddx >= ddy){  // first octant (0 <= slope <= 1)
+			// compulsory initialization (even for errorprev, needed when dx==dy)
+			errorprev = error = dx;  // start in the middle of the square
+			for (i=0 ; i < dx ; i++){  // do not use the first point (already done)
+				x += xstep;
+				error += ddy;
+				if (error > ddx){  // increment y if AFTER the middle ( > )
+					y += ystep;
+					error -= ddx;
+					// three cases (octant == right->right-top for directions below):
+					if (error + errorprev < ddx) {
+						points.add(new GridPosition(x, y-ystep));
+					} else if (error + errorprev > ddx) {
+						points.add(new GridPosition(x-xstep, y));
+					} else{  // corner: bottom and left squares also
+						points.addCornerPair(new GridPosition(x, y-ystep), new GridPosition(x-xstep, y));
 					}
-				} 
-				points.add(new GridPosition(x, y)); 
-				errorprev = error; 
+				}
+				points.add(new GridPosition(x, y));
+				errorprev = error;
 			}
-		} else {  // the same as above 
-			errorprev = error = dy; 
-			for (i=0 ; i < dy ; i++){ 
-				y += ystep; 
-				error += ddx; 
-				if (error > ddy){ 
-					x += xstep; 
-					error -= ddy; 
-					if (error + errorprev < ddy) 
-						points.add(new GridPosition(x-xstep, y)); 
-					else if (error + errorprev > ddy) 
-						points.add(new GridPosition(x, y-ystep)); 
-					else{ 
-						points.addCornerPair(new GridPosition(x-xstep, y), new GridPosition(x, y-ystep)); 
+		} else {  // the same as above
+			errorprev = error = dy;
+			for (i=0 ; i < dy ; i++){
+				y += ystep;
+				error += ddx;
+				if (error > ddy){
+					x += xstep;
+					error -= ddy;
+					if (error + errorprev < ddy) {
+						points.add(new GridPosition(x-xstep, y));
+					} else if (error + errorprev > ddy) {
+						points.add(new GridPosition(x, y-ystep));
+					} else{
+						points.addCornerPair(new GridPosition(x-xstep, y), new GridPosition(x, y-ystep));
 					}
-				} 
-				points.add(new GridPosition(x, y)); 
-				errorprev = error; 
+				}
+				points.add(new GridPosition(x, y));
+				errorprev = error;
 			}
 		}
 		points.remove(new GridPosition((int)b.x, (int)b.y));
@@ -228,7 +233,7 @@ public class BattleMap implements BattleEventLoggable {
 		}
 		return true;
 	}
-	
+
 	public boolean checkCornerPairsForSightBlockers(ArrayList<Supercover.CornerPair> list) {
 		for (Supercover.CornerPair pair : list) {
 			if (getCell(pair.a).hasTileSightBlocker() && getCell(pair.b).hasTileSightBlocker()) {
@@ -237,7 +242,7 @@ public class BattleMap implements BattleEventLoggable {
 		}
 		return true;
 	}
-	
+
 	/*
 	 * Using the centers/corners to determine line of sight between two GridPositions
 	 */
@@ -258,6 +263,6 @@ public class BattleMap implements BattleEventLoggable {
 			a.register(log);
 		}
 	}
-	
-	
+
+
 }
