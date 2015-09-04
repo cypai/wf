fbbb08dde3378ce85b4e12a275c1fd5409565bba
Charlie Pai <mail4charliepai@gmail.com>
2015-09-03 20:36:03 -0700

    Indicate when your agent has no AP

--- core/src/main/java/com/pipai/wf/guiobject/overlay/AnchoredAgentInfoDisplay.java
+++ core/src/main/java/com/pipai/wf/guiobject/overlay/AnchoredAgentInfoDisplay.java
@@ -1,10 +1,13 @@
 package com.pipai.wf.guiobject.overlay;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
+import com.pipai.wf.battle.Team;
 import com.pipai.wf.gui.BatchHelper;
 import com.pipai.wf.gui.BattleGUI;
 import com.pipai.wf.guiobject.AnchoredGUIObject;
@@ -28,6 +31,8 @@ public class AnchoredAgentInfoDisplay extends AnchoredGUIObject {
 			return;
 		}
 		ShapeRenderer shapeBatch = batch.getShapeRenderer();
+		Gdx.gl.glEnable(GL20.GL_BLEND);
+		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
 		shapeBatch.begin(ShapeType.Filled);
 		int bar_width = 40;
 		Vector2 agentPoint = screenPosition;
@@ -37,23 +42,25 @@ public class AnchoredAgentInfoDisplay extends AnchoredGUIObject {
 		Vector2 barLeftBot = new Vector2(screenPosition.x + 24, screenPosition.y + 18);
 		Vector2 barRightFullBot = new Vector2(screenPosition.x + 24 + bar_width, screenPosition.y + 18);
 		Vector2 barRightBot = new Vector2(screenPosition.x + 24 + bar_width * ((float)a.getDisplayHP() / (float)a.getAgent().getMaxHP()), screenPosition.y + 18);
-		shapeBatch.setColor(Color.BLUE);
+		float alpha = (a.getAgent().getTeam() == Team.PLAYER && a.getAgent().getAP() == 0) ? -0.5f : 0;
+		shapeBatch.setColor(Color.BLUE.cpy().add(0, 0, 0, alpha));
 		shapeBatch.rectLine(agentPoint, barLeftTop, 3);
 		// Health bar background
-		shapeBatch.setColor(Color.BLACK);
+		shapeBatch.setColor(Color.BLACK.cpy().add(0, 0, 0, alpha));
 		shapeBatch.rectLine(barLeftTop, barRightFullTop, 6);
 		shapeBatch.rectLine(barLeftBot, barRightFullBot, 6);
 		// Health bar
-		shapeBatch.setColor(Color.GRAY);
+		shapeBatch.setColor(Color.GRAY.cpy().add(0, 0, 0, alpha));
 		shapeBatch.rectLine(barLeftTop, barRightTop, 6);
-		shapeBatch.setColor(Color.GREEN);
+		shapeBatch.setColor(Color.GREEN.cpy().add(0, 0, 0, alpha));
 		shapeBatch.rectLine(barLeftBot, barRightBot, 6);
 		shapeBatch.end();
 		// Health bars outline
 		shapeBatch.begin(ShapeType.Line);
-		shapeBatch.setColor(Color.BLUE);
+		shapeBatch.setColor(Color.BLUE.cpy().add(0, 0, 0, alpha));
 		shapeBatch.rect(barLeftTop.x, barLeftTop.y + 3, barRightFullTop.x - barLeftTop.x, -12);
 		shapeBatch.end();
+		Gdx.gl.glDisable(GL20.GL_BLEND);
 		// Overwatch Icon
 		if (a.getAgent().isOverwatching()) {
 			shapeBatch.begin(ShapeType.Filled);
