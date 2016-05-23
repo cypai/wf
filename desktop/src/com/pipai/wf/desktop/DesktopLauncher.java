34ccfa37ec4a3d49c5ef2b523518414153192712
Cheng-Yu Pai <mail4charliepai@gmail.com>
2016-05-23 01:45:24 -0700

    Increase resolution

--- desktop/src/com/pipai/wf/desktop/DesktopLauncher.java
+++ desktop/src/com/pipai/wf/desktop/DesktopLauncher.java
@@ -5,10 +5,10 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.pipai.wf.WFGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 800;
-		config.height = 640;
+		config.width = 1024;
+		config.height = 768;
 		new LwjglApplication(new WFGame(), config);
 	}
 }
